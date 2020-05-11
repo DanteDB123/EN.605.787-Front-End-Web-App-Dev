877b9cd090d5a0c10a2284efd2aa32eb4534f0e2
Dante De Benedictis <debenedictisdevelopment@gmail.com>
2020-05-11 00:17:18 -0400

    reference properties of the user object in the signup controller so it isn't manipulating the same object in memory as the service prior to submitting the form

--- module10-solution/src/public/signup/signup.controller.js
+++ module10-solution/src/public/signup/signup.controller.js
@@ -8,10 +8,27 @@ SignUpController.$inject = ['user', 'saveUser'];
 function SignUpController(user, saveUser) {
   var signUpCtr = this;
 
-  signUpCtr.user = user;
+    // first check to make sure user is defined before trying to access properties of the object
+    // reference properties of the user object so it isn't manipulating the same object in memory as the service prior to submitting the form
+  if(user){
+    signUpCtr.firstName = user.firstName;
+    signUpCtr.lastName = user.lastName;
+    signUpCtr.phone = user.phone;
+    signUpCtr.email = user.email;
+    signUpCtr.favoriteDish = user.favoriteDish;
+  }
 
   signUpCtr.submit = function(){
-    saveUser(signUpCtr.user);
+    // create a new user object to be saved
+    const newUserData = {
+      firstName: signUpCtr.firstName,
+      lastName: signUpCtr.lastName,
+      email: signUpCtr.email,
+      phone: signUpCtr.phone,
+      favoriteDish: signUpCtr.favoriteDish
+    };
+
+    saveUser(newUserData);
     signUpCtr.saved = true;
   }
 }
